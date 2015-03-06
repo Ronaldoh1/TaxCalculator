3174f19c24f220f658752fd75d9f1f7a74cf05b6
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-05 22:00:44 -0800

    added compute tax method

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -11,6 +11,11 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *priceTextField;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
 
 @end
 
@@ -18,12 +23,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)onCalculateButtonTapped:(id)sender {
+    NSString *text = self.priceTextField.text;
+    double number = text.intValue;
+
+
+    if (self.segmentedControl.selectedSegmentIndex == 0) {
+
+        double results = number * self.caTax;
+
+
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", results];
+
+
+    } else if (self.segmentedControl.selectedSegmentIndex == 1){
+        double results = number * self.chiTax;
+
+
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", results];
+
+    }else if (self.segmentedControl.selectedSegmentIndex == 2){
+        double results = number * self.nyTax;
+
+
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", results];
+
+    }
 }
 
 @end
